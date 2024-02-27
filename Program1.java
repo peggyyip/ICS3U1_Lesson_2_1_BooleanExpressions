084946409f7d0b3091b9494b15834a63d7acf8ff
peggyyip <peggy.yip25@ycdsbk12.ca>
2024-02-27 11:32:19 -0500

    final

--- Program1.java
+++ Program1.java
@@ -7,13 +7,24 @@ class Program1 extends ConsoleProgram {
   
   public void run() {
     
-    boolean z = 4 < 5;
+    /*boolean z = 4 < 5;
     System.out.println("The result of 4 < 5 is " + z);
 
     int x = 4;
     int y = 5;
     boolean t = x > y;
     System.out.println("The result of " + x + " > " + y + " is " + t);
+    */
     
+    boolean blnIsLessThan = 4 < 5;
+    System.out.println("The result of 4 < 5 is " + blnIsLessThan);
+
+    int intValue1 = 4;
+    int intValue2 = 5;
+    boolean blnIsGreaterThan = intValue1 > intValue2;
+    System.out.println("The result of " + intValue1 + " > " + intValue2 + " is " + blnIsGreaterThan);
+
+
+    // name all variables something specific
   }
 }
